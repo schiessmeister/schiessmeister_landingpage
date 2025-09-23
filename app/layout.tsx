36d5af85e29e9de9844f7d8948ee83cb435b3f40
Luca Nigsch <luca@nigsch.info>
2025-09-23 16:42:51 +0200

    added google tag

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,6 +6,9 @@ import { Analytics } from "@vercel/analytics/next"
 import { Suspense } from "react"
 import "./globals.css"
 
+import { GoogleAnalytics } from '@next/third-parties/google'
+
+
 export const metadata: Metadata = {
   title: "Schiessmeister - Digitale Wettkampfauswertung",
   description: "Die smarte Lösung für Schützenvereine und ihre statischen Bewerbe. Ohne digitales Zielsystem!",
@@ -25,6 +28,7 @@ export default function RootLayout({
       <body className={`font-sans ${GeistSans.variable} ${GeistMono.variable}`}>
         <Suspense fallback={null}>{children}</Suspense>
         <Analytics />
+        <GoogleAnalytics gaId="GTM-T8NZ4674" />
       </body>
     </html>
   )

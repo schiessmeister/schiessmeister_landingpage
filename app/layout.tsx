68cb50511ab923c8cd60c7739dab4c8126bc809a
Luca Nigsch <luca@nigsch.info>
2025-10-01 20:19:24 +0200

    added meta pixel

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,6 +7,7 @@ import { Suspense } from "react"
 import "./globals.css"
 
 import { GoogleAnalytics } from '@next/third-parties/google'
+import Script from 'next/script'
 
 
 export const metadata: Metadata = {
@@ -24,6 +25,35 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="de">
+      <head>
+        {/* Facebook Pixel Code */}
+        <Script
+          id="facebook-pixel"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              !function(f,b,e,v,n,t,s)
+              {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+              n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+              if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+              n.queue=[];t=b.createElement(e);t.async=!0;
+              t.src=v;s=b.getElementsByTagName(e)[0];
+              s.parentNode.insertBefore(t,s)}(window, document,'script',
+              'https://connect.facebook.net/en_US/fbevents.js');
+              fbq('init', '775244318645138');
+              fbq('track', 'PageView');
+            `,
+          }}
+        />
+        <noscript>
+          <img
+            height="1"
+            width="1"
+            style={{ display: 'none' }}
+            src="https://www.facebook.com/tr?id=775244318645138&ev=PageView&noscript=1"
+          />
+        </noscript>
+      </head>
       <body className={`font-sans ${GeistSans.variable} ${GeistMono.variable}`}>
         <Suspense fallback={null}>{children}</Suspense>
         <Analytics />
